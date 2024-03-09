738fd1309a49ae968136e24309e4455e7c04afd7
CoolShivas <shivas2710cool00@gmail.com>
2024-03-09 15:19:40 +0530

    First_Step_Of_UseReducer

--- Sellers_Admin_Page/src/App.jsx
+++ Sellers_Admin_Page/src/App.jsx
@@ -1,34 +1,60 @@
 import { AuthContext } from "./store/AuthContext";
 import ErrorMsg from "./components/ErrorMsg";
 import AllProducts from "./components/AllProducts";
-import { useState } from "react";
+import { useReducer, useState } from "react";
 import InputField from "./components/InputField";
 import AppName from "./components/AppName";
 
 
+
+const inputReducer = (currState, action) => {
+
+  let newProductList = currState;
+  if (action.type === "ADD_PRO") {
+    newProductList = [...currState,
+    {
+      productId: action.payload.productIdRST,
+      sellingPrice: action.payload.sellingPriceRST,
+      productName: action.payload.productNameRST,
+      category: action.payload.categoryRST,
+    }]
+  }
+  else if (action.type === "DELETE_PRO") {
+    newProductList = currState.filter((brr) => {
+      return brr.productName !== action.payload.del;
+    })
+  }
+  return newProductList;
+}
+
+
 function App() {
 
-  const [inputValue, setInputValue] = useState([]);
+  // const [inputValue, setInputValue] = useState([]);
+  const [inputValue, dispatchInputValue] = useReducer(inputReducer, []);
+
 
   const handlerOnAddProducts = (productIdRST, sellingPriceRST, productNameRST, categoryRST) => {
     console.log(`All details are ${productIdRST} - ${sellingPriceRST} - ${productNameRST} - ${categoryRST} `);
 
-    const productDetails = [...inputValue,
-    {
-      productId: productIdRST,
-      sellingPrice: sellingPriceRST,
-      productName: productNameRST,
-      category: categoryRST,
-    }]
-    setInputValue(productDetails);
+    const productDetails = {
+      type: "ADD_PRO",
+      payload: {
+        productIdRST, sellingPriceRST, productNameRST, categoryRST,
+      }
+    }
+    dispatchInputValue(productDetails);
   }
 
   const handlerOnDeleteProducts = (del) => {
-    const deletePro = inputValue.filter((brr) => {
-      return brr.productName !== del;
-    })
-    setInputValue(deletePro);
+
+    const deletePro = {
+      type: "DELETE_PRO",
+      payload: { del }
+    }
+    dispatchInputValue(deletePro);
   }
+
 
   return <AuthContext.Provider value={{
     inputValue,
