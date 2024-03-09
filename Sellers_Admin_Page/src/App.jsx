e6c144243a9b444e70e854ab4ac80a1c389b03c4
CoolShivas <shivas2710cool00@gmail.com>
2024-03-09 15:52:10 +0530

    Third_Step_of_Context_API

--- Sellers_Admin_Page/src/App.jsx
+++ Sellers_Admin_Page/src/App.jsx
@@ -1,4 +1,4 @@
-import { AuthContext } from "./store/AuthContext";
+import AuthContextProvider from "./store/AuthContext";
 import ErrorMsg from "./components/ErrorMsg";
 import AllProducts from "./components/AllProducts";
 import { useReducer, useState } from "react";
@@ -6,61 +6,10 @@ import InputField from "./components/InputField";
 import AppName from "./components/AppName";
 
 
-
-const inputReducer = (currState, action) => {
-
-  let newProductList = currState;
-  if (action.type === "ADD_PRO") {
-    newProductList = [...currState,
-    {
-      productId: action.payload.productIdRST,
-      sellingPrice: action.payload.sellingPriceRST,
-      productName: action.payload.productNameRST,
-      category: action.payload.categoryRST,
-    }]
-  }
-  else if (action.type === "DELETE_PRO") {
-    newProductList = currState.filter((brr) => {
-      return brr.productName !== action.payload.del;
-    })
-  }
-  return newProductList;
-}
-
-
 function App() {
 
-  // const [inputValue, setInputValue] = useState([]);
-  const [inputValue, dispatchInputValue] = useReducer(inputReducer, []);
 
-
-  const handlerOnAddProducts = (productIdRST, sellingPriceRST, productNameRST, categoryRST) => {
-    console.log(`All details are ${productIdRST} - ${sellingPriceRST} - ${productNameRST} - ${categoryRST} `);
-
-    const productDetails = {
-      type: "ADD_PRO",
-      payload: {
-        productIdRST, sellingPriceRST, productNameRST, categoryRST,
-      }
-    }
-    dispatchInputValue(productDetails);
-  }
-
-  const handlerOnDeleteProducts = (del) => {
-
-    const deletePro = {
-      type: "DELETE_PRO",
-      payload: { del }
-    }
-    dispatchInputValue(deletePro);
-  }
-
-
-  return <AuthContext.Provider value={{
-    inputValue,
-    handlerOnAddProducts,
-    handlerOnDeleteProducts,
-  }}>
+  return <AuthContextProvider>
     <center>
       <AppName></AppName>
       <InputField
@@ -74,7 +23,7 @@ function App() {
       // inputValueABC={inputValue}
       ></ErrorMsg>
     </center>
-  </AuthContext.Provider>
+  </AuthContextProvider>
 }
 
 export default App;
--- Sellers_Admin_Page/src/store/AuthContext.jsx
+++ Sellers_Admin_Page/src/store/AuthContext.jsx
@@ -1,3 +1,4 @@
+import { useReducer } from "react";
 import { createContext } from "react";
 
 export const AuthContext = createContext({
@@ -5,3 +6,69 @@ export const AuthContext = createContext({
     handlerOnAddProducts: () => { },
     handlerOnDeleteProducts: () => { },
 });
+
+
+
+
+const inputReducer = (currState, action) => {
+
+    let newProductList = currState;
+    if (action.type === "ADD_PRO") {
+        newProductList = [...currState,
+        {
+            productId: action.payload.productIdRST,
+            sellingPrice: action.payload.sellingPriceRST,
+            productName: action.payload.productNameRST,
+            category: action.payload.categoryRST,
+        }]
+    }
+    else if (action.type === "DELETE_PRO") {
+        newProductList = currState.filter((brr) => {
+            return brr.productName !== action.payload.del;
+        })
+    }
+    return newProductList;
+}
+
+
+
+
+
+const AuthContextProvider = ({ children }) => {
+
+    const [inputValue, dispatchInputValue] = useReducer(inputReducer, []);
+
+
+    const handlerOnAddProducts = (productIdRST, sellingPriceRST, productNameRST, categoryRST) => {
+        console.log(`All details are ${productIdRST} - ${sellingPriceRST} - ${productNameRST} - ${categoryRST} `);
+
+        const productDetails = {
+            type: "ADD_PRO",
+            payload: {
+                productIdRST, sellingPriceRST, productNameRST, categoryRST,
+            }
+        }
+        dispatchInputValue(productDetails);
+    }
+
+    const handlerOnDeleteProducts = (del) => {
+
+        const deletePro = {
+            type: "DELETE_PRO",
+            payload: { del }
+        }
+        dispatchInputValue(deletePro);
+    }
+
+    return <AuthContext.Provider value={
+        {
+            inputValue,
+            handlerOnAddProducts,
+            handlerOnDeleteProducts,
+        }
+    }>
+        {children}
+    </AuthContext.Provider>
+}
+
+export default AuthContextProvider;
